95987a119d318a598f163a3dd1bccbe7530cba87
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2023-06-12 16:04:30 -0700

    Added unit and integration testing

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -13,6 +13,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="NumberOfEvents">
+        <label>Number of Events: </label>
         <input
           type="number"
           className="number"
