6c68ae4c8fabb9497b25838992fbdf1f6caba5af
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2023-06-19 20:38:06 -0700

    Added data chart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import "./nprogress.css";
 import { ErrorAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
 
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
 class App extends Component {
   state = {
     events: [],
@@ -70,12 +79,25 @@ class App extends Component {
       this.setState({ infoText: "" });
     }
   };
-
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
   render() {
+    // const { locations, numberOfEvents } = this.state;
+
     if (this.state.showWelcomeScreen === undefined)
       return <div className="App" />;
     return (
       <div className="App">
+        <h1>Meet App</h1>
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
@@ -83,9 +105,26 @@ class App extends Component {
         <NumberOfEvents
           // numberOfEvents={this.state.numberOfEvents}
           updateEvents={this.updateEvents}
-        />
+        />{" "}
+        <h4>Events in each city</h4>
         <ErrorAlert text={this.state.infoText} />
+        <ScatterChart
+          width={800}
+          height={400}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis dataKey="city" type="category" name="city" />
+          <YAxis dataKey="number" type="number" name="number of events" />
 
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
         <EventList events={this.state.events} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
